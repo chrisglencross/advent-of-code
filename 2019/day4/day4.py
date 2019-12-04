315bc0040b343e602d97274ec920dfa492829e33
Chris Glencross <chris@glencross.org>
2019-12-03 21:13:36 -0800

    2019 day 4

---
+++ 2019/day4/day4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Advent of code 2019 day 4
+
+import itertools
+
+count = 0
+for i in range(123257, 647016):
+    password = str(i)
+
+    previous_char = None
+    increase = True
+    for char in password:
+        if previous_char is not None:
+            if ord(previous_char) > ord(char):
+                increase = False
+                break
+        previous_char = char
+
+    contains_pair = False
+    for c, consecutive_chars in itertools.groupby(password):
+        if len(list(consecutive_chars)) == 2:
+            contains_pair = True
+
+    if increase and contains_pair:
+        count = count + 1
+
+print(count)
