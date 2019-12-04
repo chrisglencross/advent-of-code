615662f8a1d0db6643002edd5eabad29510a8012
Chris Glencross <chris@glencross.org>
2019-12-04 09:40:04 -0800

    Simpler solution

--- 2019/day4/day4.py
+++ 2019/day4/day4.py
@@ -7,21 +7,14 @@ count = 0
 for i in range(123257, 647016):
     password = str(i)
 
-    previous_char = None
-    increase = True
-    for char in password:
-        if previous_char is not None:
-            if ord(previous_char) > ord(char):
-                increase = False
-                break
-        previous_char = char
+    is_sorted = "".join(sorted(password)) == password
 
     contains_pair = False
     for c, consecutive_chars in itertools.groupby(password):
         if len(list(consecutive_chars)) == 2:  # >=2 for part 1
             contains_pair = True
 
-    if increase and contains_pair:
+    if is_sorted and contains_pair:
         count = count + 1
 
 print(count)
