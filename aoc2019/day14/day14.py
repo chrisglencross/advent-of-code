996250ba987672c80f8c499db75f316768e102d8
Chris Glencross <chris@glencross.org>
2019-12-14 15:27:33 -0500

    Tidy up with comments

--- aoc2019/day14/day14.py
+++ aoc2019/day14/day14.py
@@ -8,7 +8,6 @@ from typing import List
 
 @dataclass
 class Recipe:
-    name: str
     product: str
     product_quantity: int
     requires: dict
@@ -28,45 +27,58 @@ def load_recipes():
                 requires[parts[1]] = int(parts[0])
             produces_qty = int(match.group(2))
             produces = match.group(3)
-            recipes[produces] = Recipe(line.strip(), produces, produces_qty, requires)
+            recipes[produces] = Recipe(produces, produces_qty, requires)
     return recipes
 
 
-def build_dependencies(recipes: List[Recipe]):
+def calc_transitive_dependencies(recipes: List[Recipe]):
+    """Returns a dict: product -> set of transitive dependency products required to create this product."""
     result = {}
 
+    # Populate result with direct dependencies
     for receipe in recipes:
         result[receipe.product] = set([dependency for dependency in receipe.requires.keys()])
 
+    # Iteratively add missing transitive dependencies to each set of dependencies until no more changes
+    # Could be more efficient, but does not need to be.
     modified = True
     while modified:
         modified = False
-        for p, ds in result.items():
-            for d in list(ds):
-                add = result.get(d, set())
-                if not add.issubset(ds):
+        for product, dependencies in result.items():
+            for dependency in list(dependencies):
+                transitive_dependencies = result.get(dependency, set())
+                if not transitive_dependencies.issubset(dependencies):
                     modified = True
-                    ds.update(add)
+                    dependencies.update(transitive_dependencies)
 
     return result
 
 
-def get_sorted_products(dependencies):
+def get_sorted_products(recipes):
+    """Returns products in reverse order of manufacture, with ORE at the end and FUEL at the start."""
     sorted_products = ["ORE"]
-    remaining = dict(dependencies)
-    while remaining:
-        for p, ds in dict(remaining).items():
-            if set(ds).issubset(sorted_products):
-                sorted_products.append(p)
-                del remaining[p]
+
+    dependencies = calc_transitive_dependencies(recipes.values())
+    products_to_insert = set(dependencies.keys())
+
+    # Iteratively appends product to the sorted list if all dependencies are already in the list
+    while products_to_insert:
+        for product in set(products_to_insert):
+            if dependencies[product].issubset(sorted_products):
+                sorted_products.insert(0, product)
+                products_to_insert.remove(product)
+
     return sorted_products
 
 
 def ore_required(fuel_count):
+    # dictionary of product quantities that we need to produce
     requirements = {"FUEL": fuel_count}
 
+    # produce dependencies in order
     for product in sorted_products:
 
+        # ignore ORE, it has no dependencies
         if product == "ORE":
             continue
 
@@ -79,13 +91,15 @@ def ore_required(fuel_count):
         for recipe_product, recipe_quantity in recipe.requires.items():
             requirements[recipe_product] = requirements.get(recipe_product, 0) + (recipe_quantity * use_recipe_times)
             debug_text.append(f"{recipe_quantity * use_recipe_times} {recipe_product}")
+
+        del requirements[product]
         # print(f"Consume {', '.join(debug_text)} to produce {requirement} {product}")
 
     return requirements["ORE"]
 
 
-# Finds the amount of fuel that can be produced by max_ore using a binary search
 def binary_search(max_ore, lo=1, hi=None):
+    """Finds the amount of fuel that can be produced using max_ore with a binary search."""
     while hi is None or lo < hi:
         if hi is None:
             # No upper limit yet; try guessing double the previous value
@@ -106,11 +120,9 @@ def binary_search(max_ore, lo=1, hi=None):
 
 
 if __name__ == "__main__":
+    # Init global variables
     recipes = load_recipes()
-    dependencies = build_dependencies(recipes.values())
-
-    sorted_products = get_sorted_products(dependencies)
-    sorted_products.reverse()
+    sorted_products = get_sorted_products(recipes)
 
     # Part 1
     ore_required_for_one_fuel = ore_required(1)
