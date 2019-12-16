a2392614901c0c866c432151ba438c8869b597bf
Chris Glencross <chris@glencross.org>
2019-12-16 17:39:44 -0500

    2019 day 16 - another horrible one.

--- aoc2019/day16/day16.py
+++ aoc2019/day16/day16.py
@@ -48,7 +48,7 @@ values = [int(value) for value in line] * 10000
 
 for phase in range(100):
     digit_value = 0
-    new_values = [int(value) for value in line] * 10000
+    new_values = [None] * len(values)
     for digit_pos in range(len(values) - 1, offset - 1, -1):
         digit_value += (values[digit_pos]) % 10
         digit_value %= 10
