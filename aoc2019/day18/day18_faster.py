d1cd01423a5df96c7a2ed1062dac467c2e5555df
Chris Glencross <chris@glencross.org>
2019-12-18 22:55:27 +0000

    Faster version which doesn't work with graphs containing cycles.

---
+++ aoc2019/day18/day18_faster.py
@@ -0,0 +1,224 @@
+#!/usr/bin/python3
+# Advent of code 2019 day 18
+
+# Faster version which doesn't work with graphs containing cycles.
+# On my laptop part 1 takes 30 seconds, vs. 135 seconds with the previous version.
+
+import timeit
+from dataclasses import dataclass
+from typing import List, Set
+
+import networkx as nx
+
+DIRECTIONS = {
+    "north": (0, -1),
+    "east": (1, 0),
+    "south": (0, 1),
+    "west": (-1, 0),
+}
+
+
+@dataclass
+class RouteState:
+    locations: List[str]  # multiple droid locations for part 2
+    route_length: int
+    collected_list: List[str]
+    targets: Set[str]
+
+    def get_state_key(self):
+        return tuple(self.locations), tuple(sorted(self.collected_list))
+
+
+@dataclass
+class RouteInfo:
+    path: List[str]
+    route_len: int
+
+
+def get_neighbours(x, y):
+    result = set()
+    for move in DIRECTIONS.values():
+        result.add((x + move[0], y + move[1]))
+    return result
+
+
+def find_direct_distances(grid, start_location):
+    route_lengths = {}
+    check_neighbours = set()
+    route_lengths[start_location] = 0
+    check_neighbours.add(start_location)
+    while check_neighbours:
+        next_check_neighbours = set()
+        for location in list(check_neighbours):
+            location_distance = route_lengths[location]
+            for neighbour in get_neighbours(location[0], location[1]):
+                if neighbour in route_lengths.keys():
+                    continue
+                if grid[neighbour] == "#":
+                    continue
+                route_lengths[neighbour] = location_distance + 1
+                if grid[neighbour] == "." or grid[neighbour].isdigit():
+                    next_check_neighbours.add(neighbour)
+        check_neighbours = next_check_neighbours
+    return route_lengths
+
+
+def get_all_direct_distances(grid, start_locations, target_locations):
+    """This is used to create a cache of the distances between all directly navigable
+    (i.e. without moving through another object) objects of interest. Uses a possibly inefficient flood fill, but
+    only used once on start-up. This is used to build a networkx graph for the proper route finding.."""
+    direct_distances = dict()
+    for from_target, from_location in sorted(list(target_locations.items()) + list(start_locations.items())):
+        route_lengths = find_direct_distances(grid, from_location)
+        for to_target, to_location in sorted(target_locations.items()):
+            distance = route_lengths.get(to_location)
+            if distance is not None:
+                direct_distances[(from_target, to_target)] = distance
+    return direct_distances
+
+
+def build_route_graph(direct_distances):
+    graph = nx.DiGraph()
+    for (from_target, to_target), distance in direct_distances.items():
+        if to_target.isdigit():
+            # No point heading back to a start location
+            continue
+        graph.add_edge(from_target, to_target, distance=distance)
+    return graph
+
+
+def get_next_route_states(route_state, route_cache):
+    result = []
+
+    # Try moving all droids
+    for droid in range(0, len(route_state.locations)):
+
+        # Try navigating to all available keys and doors
+        for target in route_state.targets:
+
+            if target.isupper() and target.lower() in route_state.targets:
+                # Will not navigate to a door if we do not have the key
+                continue
+
+            # Cannot navigate to anything if there's no route
+            route_info = route_cache.get((route_state.locations[droid], target))
+            if route_info is None:
+                continue
+            # Cannot or will not navigate route if there is an intermediate target in the way that we haven't collected
+            if not route_state.targets.isdisjoint(route_info.path[1:-1]):
+                continue
+
+            # Create a new state object representing the moved droid and its effects
+            new_locations = list(route_state.locations)
+            new_locations[droid] = target
+            new_targets = set(route_state.targets)
+            new_targets.remove(target)
+            result.append(RouteState(locations=new_locations,
+                                     route_length=route_state.route_length + route_info.route_len,
+                                     collected_list=route_state.collected_list + [target],
+                                     targets=new_targets))
+
+    return result
+
+
+def path_length(G, path, weight):
+    length = 0
+    for i, node in enumerate(path[1:]):
+        prev = path[i]
+        length += G[prev][node][weight]
+    return length
+
+
+def find_all_routes(route_cache, start_locations, targets):
+    initial_state = RouteState(locations=start_locations, route_length=0, targets=targets, collected_list=[])
+    all_keys = set([target for target in targets if target.islower()])
+
+    best_routes_by_state = {initial_state.get_state_key(): initial_state}
+    route_states_to_check = [initial_state]
+    best_complete_route_length = None
+
+    # Travelling salesman-like problem - shortest route to visit all targets (i.e. to collect all keys)
+    # Fortunately we have locked doors to help limit the exponential nature; implemented as a breadth-first
+    # search with pruning, but it can still take 1-2 minutes for part 2, depending on speed.
+    iterations = 0
+    while route_states_to_check:
+        prune_count = 0
+        print(f"Iteration {iterations} has {len(route_states_to_check)} active states to be checked")
+        route_states_to_check.sort(key=lambda r: r.route_length)
+        # for x in route_states_to_check:
+        #     print(f"    {x.get_state_key()} {x.route_length}")
+        # print()
+        iterations += 1
+        new_route_states_to_check = []
+        for route_state in route_states_to_check:
+            next_route_states = get_next_route_states(route_state, route_cache)
+            for next_route_state in next_route_states:
+                if best_complete_route_length and next_route_state.route_length >= best_complete_route_length:
+                    prune_count += 1
+                    continue
+                state_key = next_route_state.get_state_key()
+                already_found = best_routes_by_state.get(state_key)
+                if already_found and already_found.route_length <= next_route_state.route_length:
+                    prune_count += 1
+                    continue
+                best_routes_by_state[state_key] = next_route_state
+                new_route_states_to_check.append(next_route_state)
+
+                if all_keys.isdisjoint(next_route_state.targets):
+                    best_complete_route_length = next_route_state.route_length
+
+        route_states_to_check = new_route_states_to_check
+
+    # We can end up with one completed route per distinct final state: less than 5 for all the examples
+    # Return all of them sorted by length. We really just need the shortest.
+    complete_routes = []
+    for best_route in best_routes_by_state.values():
+        if all_keys.isdisjoint(best_route.targets):
+            complete_routes.append(best_route)
+    complete_routes.sort(key=lambda r: r.route_length)
+    return complete_routes
+
+
+def load_grid(file):
+    grid = {}
+    start_locations = {}
+    target_locations = {}
+    with open(file) as f:
+        lines = f.readlines()
+    for y, line in enumerate(lines):
+        for x, cell in enumerate(line.strip()):
+            grid[(x, y)] = cell
+            if cell == "@":
+                start_symbol = str(len(start_locations))
+                start_locations[start_symbol] = (x, y)
+                grid[(x, y)] = start_symbol
+            elif cell.isalpha():
+                target_locations[cell] = (x, y)
+    return grid, start_locations, target_locations
+
+
+def process_file(input_file):
+    grid, start_locations, target_locations = load_grid(input_file)
+    direct_distances = get_all_direct_distances(grid, start_locations, target_locations)
+
+    graph = build_route_graph(direct_distances)
+    route_cache = dict()
+    for source, target_path in nx.shortest_path(graph, None, None, "distance").items():
+        for target, path in target_path.items():
+            route_cache[(source, target)] = RouteInfo(path, path_length(graph, path, "distance"))
+
+    results = find_all_routes(route_cache, list(start_locations.keys()), set(target_locations.keys()))
+    print(results[0].route_length)
+
+
+def part1():
+    process_file("input_part1.txt")
+
+
+def part2():
+    process_file("input_part2.txt")
+
+
+if __name__ == "__main__":
+    print("Part 1 elapsed time:", timeit.timeit(part1, number=1))
+    print("Part 2 elapsed time:", timeit.timeit(part2, number=1))
