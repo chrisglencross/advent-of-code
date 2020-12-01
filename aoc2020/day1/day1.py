0ff5c9178d8eae6022d7ecce11daa89ac25f7e4b
Chris <chris@glencross.org>
2020-12-01 11:19:42 +0000

    2020 Day 1

--- aoc2020/day1/day1.py
+++ aoc2020/day1/day1.py
@@ -3,14 +3,16 @@
 # See https://adventofcode.com/2020/day/1
 
 import itertools
+from functools import reduce
+from operator import __mul__
 
 with open("input.txt") as f:
     values = [int(line) for line in f.readlines()]
 
-for pairs in itertools.combinations(values, 2):
-    if sum(pairs) == 2020:
-        print(pairs[0] * pairs[1])
 
-for triples in itertools.combinations(values, 3):
-    if sum(triples) == 2020:
-        print(triples[0] * triples[1] * triples[2])
+def solve(items):
+    return [reduce(__mul__, c) for c in itertools.combinations(values, items) if sum(c) == 2020]
+
+
+print(solve(2))
+print(solve(3))
