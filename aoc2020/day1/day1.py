f40a42908be8fba944f44dac134902c2395eb0d7
Chris <chris@glencross.org>
2020-12-02 18:10:03 +0000

    Tidy

--- aoc2020/day1/day1.py
+++ aoc2020/day1/day1.py
@@ -3,15 +3,15 @@
 # See https://adventofcode.com/2020/day/1
 
 import itertools
+from operator import mul
 from functools import reduce
-from operator import __mul__
 
 with open("input.txt") as f:
     values = [int(line) for line in f.readlines()]
 
 
 def solve(items):
-    return [reduce(__mul__, c) for c in itertools.combinations(values, items) if sum(c) == 2020]
+    return [reduce(mul, c) for c in itertools.combinations(values, items) if sum(c) == 2020]
 
 
 print(solve(2))
