9cf5570e2c4a1f8d684589b9ab012f9ad1654e50
Chris <chris@glencross.org>
2020-12-02 14:32:25 +0000

    Learning Kotlin

--- aoc2020-kotlin/src/day1/Day1.kt
+++ aoc2020-kotlin/src/day1/Day1.kt
@@ -9,12 +9,12 @@ fun main() {
     fun solve(values: List<Int>, select: Int): List<Int> {
         return combinations(values, select)
                 .filter { it.sum() == 2020 }
-                .map { it.reduce { x: Int, y: Int -> x * y } }
+                .map { it.reduce { x, y -> x * y } }
                 .toList()
     }
 
     val lines = Files.readAllLines(Paths.get("aoc2020/day1/input.txt"))
-    val values = lines.map { Integer.parseInt(it) }.toList()
+    val values = lines.map { it.toInt() }
 
     println(solve(values, 2))
     println(solve(values, 3))
