89f57bf0d0c83d9001037b2e9ad6a4c13928b40b
Chris <chris@glencross.org>
2020-12-02 11:16:25 +0000

    2020 Day 2

--- aoc2020/day2/day2.py
+++ aoc2020/day2/day2.py
@@ -6,30 +6,26 @@ import re
 import itertools
 
 
-def is_valid1(minimum, maximum, char, password):
+def is_valid1(n1, n2, char, password):
     counts = dict([(c, len(list(cs))) for c, cs in itertools.groupby(sorted(password))])
-    return minimum <= counts.get(char, 0) <= maximum
+    return n1 <= counts.get(char, 0) <= n2
 
 
-def is_valid2(minimum, maximum, char, password):
-    return (password[minimum-1] == char) != (password[maximum-1] == char)
+def is_valid2(n1, n2, char, password):
+    return (password[n1 - 1] == char) != (password[n2 - 1] == char)
 
 
-def count_valid(lines, is_valid):
+def count_valid(is_valid):
     valid = 0
     for line in lines:
         match = re.search("^([0-9]+)-([0-9]+) (.): (.*)$", line.strip())
-        if match:
-            minimum = int(match.group(1))
-            maximum = int(match.group(2))
-            char = match.group(3)
-            password = match.group(4)
-            if is_valid(minimum, maximum, char, password):
-                valid += 1
+        if match and is_valid(int(match.group(1)), int(match.group(2)), match.group(3),  match.group(4)):
+            valid += 1
     return valid
 
 
 with open("input.txt") as f:
     lines = f.readlines()
-print("Part 1:", count_valid(lines, is_valid1))
-print("Part 2:", count_valid(lines, is_valid2))
+
+print("Part 1:", count_valid(is_valid1))
+print("Part 2:", count_valid(is_valid2))
