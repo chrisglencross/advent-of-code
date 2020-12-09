719ec4dbb551accd12e2f8c7654ffda7fc7d78e7
Chris <chris@glencross.org>
2020-12-09 07:40:13 +0000

    2020 Day 9

---
+++ aoc2020/day9/day9.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Advent of code 2020 day 9
+# See https://adventofcode.com/2020/day/9
+
+preamble = 25
+with open("input.txt") as f:
+    lines = f.readlines()
+values = [int(line) for line in lines]
+
+for i in range(preamble, len(values)):
+    v = values[i]
+    previous = values[i-preamble: i]
+    if len([p for p in previous if (v - p) in previous]) < 2:
+        print(f"{i}: {v} not in {previous}")
+        break
+
+for j in range(0, i):
+    for c in range(2, i-j):
+        r = values[j:j+c]
+        t = sum(r)
+        if t == v:
+            print(min(r) + max(r))
+        elif t > v:
+            break
