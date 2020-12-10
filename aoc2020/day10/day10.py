67a75b16c19ced97e94a900897ad461a561c8194
Chris <chris@glencross.org>
2020-12-10 08:57:50 +0000

    2020 Day 10

--- aoc2020/day10/day10.py
+++ aoc2020/day10/day10.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 # Advent of code 2020 day 10
 # See https://adventofcode.com/2020/day/10
-import itertools
+from itertools import groupby
 from functools import cache
 
 with open("input.txt") as f:
-    values = sorted([0] + [int(line) for line in f.readlines()])
+    adapters = [int(line) for line in f.readlines()]
+jolts = [0] + sorted(adapters) + [max(adapters) + 3]
 
-# Part 1
-groups = dict([(k, len(list(v))) for k, v in itertools.groupby(sorted([n-p for p, n in zip(values, values[1:] + [values[-1]+3])]))])
-print(groups[1] * groups[3])
 
-# Part 2
-@cache
-def count_options(prev, remaining):
-    if len(remaining) <= 1:
-        return 1
-    options = count_options(remaining[0], remaining[1:])
-    if remaining[1] - prev <= 3:
-        options += count_options(prev, remaining[1:])
-    return options
+def part1():
+    diffs = [j2 - j1 for j1, j2 in zip(jolts[:-1], jolts[1:])]
+    groups = dict([(k, len(list(v))) for k, v in groupby(sorted(diffs))])
+    print(groups[1] * groups[3])
 
-print(count_options(values[0], tuple(values[1:])))
+
+def part2():
+    @cache
+    def count_options(prev, remaining):
+        if len(remaining) <= 1:
+            return 1
+        options = count_options(remaining[0], remaining[1:])
+        if remaining[1] - prev <= 3:
+            options += count_options(prev, remaining[1:])
+        return options
+    print(count_options(jolts[0], tuple(jolts[1:])))
+
+
+part1()
+part2()
