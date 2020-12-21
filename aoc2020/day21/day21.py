3bf4e2ed1320120ea2f4ec9bc2d1d741a187a46a
Chris <chris@glencross.org>
2020-12-21 08:24:42 +0000

    2020 day 21.

--- aoc2020/day21/day21.py
+++ aoc2020/day21/day21.py
@@ -4,40 +4,63 @@
 
 import re
 
-with open("input.txt") as f:
-    lines = f.readlines()
+with open("input.txt") as file:
+    lines = file.readlines()
 
-all_ingredients = []
-possible_allergen_foods = {}
-for line in lines:
-    match = re.fullmatch(r"(.*) \(contains (.*)\)", line.strip().replace(",", ""))
-    food_ingredients, food_allergens = [m.split(" ") for m in match.groups()]
-    all_ingredients.extend(food_ingredients)
-    for food_allergen in food_allergens:
-        possible_foods = possible_allergen_foods.get(food_allergen)
-        if possible_foods is None:
-            possible_allergen_foods[food_allergen] = set(food_ingredients)
-        else:
-            possible_foods.intersection_update(food_ingredients)
 
-no_allergens = set()
-for ingredient in set(all_ingredients):
-    if not any([a for a, i in possible_allergen_foods.items() if ingredient in i]):
-        no_allergens.add(ingredient)
+def load():
+    result = []
+    for line in lines:
+        match = re.fullmatch(r"(.*) \(contains (.*)\)", line.strip().replace(",", ""))
+        ingredients, allergens = [m.split(" ") for m in match.groups()]
+        result.append((ingredients, allergens))
+    return result
 
-print("Part 1:", len([ingredient for ingredient in all_ingredients if ingredient in no_allergens]))
 
-modified = True
-while modified:
-    modified = False
-    for food, possible_allergens in possible_allergen_foods.items():
-        if len(possible_allergens) == 1:
-            allergen = list(possible_allergens)[0]
-            # allergen definitely in this food, remove possibility of this allergen from all other foods
-            for other_food, other_food_allergens in possible_allergen_foods.items():
-                if food != other_food and allergen in other_food_allergens:
-                    other_food_allergens.remove(allergen)
-                    modified = True
+def get_all_ingredients(file):
+    result = []
+    for ingredients, allergens in file:
+        result.extend(ingredients)
+    return result
 
-food_allergens = [(list(i)[0], f) for f, i in possible_allergen_foods.items()]
-print("Part 2:", ",".join([p[0] for p in sorted(food_allergens, key=lambda p: p[1])]))
+
+def get_possible_allergen_ingredients(file):
+    result = {}
+    for ingredients, allergens in file:
+        for allergen in allergens:
+            possible_ingredients = result.get(allergen)
+            if possible_ingredients is None:
+                result[allergen] = set(ingredients)
+            else:
+                possible_ingredients.intersection_update(ingredients)
+    return result
+
+
+def part1(data):
+    all_ingredients = get_all_ingredients(data)
+    possible_allergen_ingredients = get_possible_allergen_ingredients(data)
+    no_allergens = [ingredient
+                    for ingredient in set(all_ingredients)
+                    if not any([a for a, i in possible_allergen_ingredients.items() if ingredient in i])]
+    print("Part 1:", len([ingredient for ingredient in all_ingredients if ingredient in no_allergens]))
+
+
+def part2(data):
+    possible_allergen_ingredients = get_possible_allergen_ingredients(data)
+    # while any allergen has more than one possible ingredient
+    while any([a for a, i in possible_allergen_ingredients.items() if len(i) > 1]):
+        for allergen, possible_ingredients in possible_allergen_ingredients.items():
+            if len(possible_ingredients) == 1:
+                ingredient = list(possible_ingredients)[0]
+                # this ingredient is definitely source of allergen, remove possibility from all other ingredients
+                for other_allergen, other_ingredients in possible_allergen_ingredients.items():
+                    if allergen != other_allergen and ingredient in other_ingredients:
+                        other_ingredients.remove(ingredient)
+
+    allergens = [(list(i)[0], f) for f, i in possible_allergen_ingredients.items()]
+    print("Part 2:", ",".join([p[0] for p in sorted(allergens, key=lambda p: p[1])]))
+
+
+data = load()
+part1(data)
+part2(data)
