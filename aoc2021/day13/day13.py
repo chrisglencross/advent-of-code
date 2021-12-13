1cca9deac4d6fe1bf3af4867c79415bbd0cf201b
Chris Glencross <chris@glencross.org>
2021-12-13 08:16:00 +0000

    2021 day 13 - code golf-ify it a little

--- aoc2021/day13/day13.py
+++ aoc2021/day13/day13.py
@@ -7,7 +7,7 @@ from functools import reduce
 from aoc2021.modules import grid as g
 
 with open("input.txt") as f:
-    blocks = f.read().replace("\r", "").split("\n\n")
+    blocks = f.read().split("\n\n")
     paper = set((int(x), int(y)) for x, y in [line.split(",") for line in blocks[0].split("\n")])
     instructions = [(xy, int(n))
                     for line in blocks[1].split("\n")
@@ -16,25 +16,12 @@ with open("input.txt") as f:
 
 def fold(paper, instruction):
     xy, line_no = instruction
-    new_paper = set()
-    for x, y in paper:
-        if xy == 'y':
-            if y < line_no:
-                new_paper.add((x, y))
-            elif y > line_no:
-                new_paper.add((x, 2 * line_no - y))
-        elif xy == 'x':
-            if x < line_no:
-                new_paper.add((x, y))
-            elif x > line_no:
-                new_paper.add((2 * line_no - x, y))
-    return new_paper
+    return set(((x, 2 * line_no - y if y > line_no else y) if xy == 'y' else (2 * line_no - x if x > line_no else x, y))
+               for x, y in paper)
 
 
 # Part 1
 print(len(fold(paper, instructions[0])))
 
 # Part 2
-paper = reduce(fold, instructions, paper)
-grid = g.Grid(dict((coords, '#') for coords in paper))
-grid.print()
+g.Grid(dict((coords, '#') for coords in reduce(fold, instructions, paper))).print()
