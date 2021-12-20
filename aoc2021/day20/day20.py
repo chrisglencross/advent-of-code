691c1e2f4be635ecd4fe5a98bfd0a20e9096aa11
Chris Glencross <chris@glencross.org>
2021-12-20 21:22:13 +0000

    2021 day 20. A bit cleaner.

--- aoc2021/day20/day20.py
+++ aoc2021/day20/day20.py
@@ -11,32 +11,26 @@ algo = lines[0]
 grid = g.parse_grid("\n".join(lines[2:]))
 
 
-def tick(grid, algo):
+def tick(grid, background, algo):
     new_grid = g.Grid({})
-    # Plus and minus infinity...
-    for y in range(-100, 201):
-        for x in range(-100, 201):
+    (min_x, min_y), (max_x, max_y) = grid.get_bounds()
+    for y in range(min_y-1, max_y+1):
+        for x in range(min_x-1, max_x+1):
             index = 0
             for dy in range(-1, 2):
                 for dx in range(-1, 2):
-                    cell = grid.get((x+dx, y+dy))
+                    cell = grid.get((x+dx, y+dy), background)
                     index = 2 * index + (1 if cell == '#' else 0)
             new_value = algo[index]
             new_grid[(x, y)] = new_value
-    return new_grid
+    background = algo[0] if background == '.' else algo[511]
+    return new_grid, background
 
 
+# background is content of cells in the infinite space beyond grid
+background = '.'
 for i in range(0, 50):
-    grid = tick(grid, algo)
-
-    # Hacky patch-up of the corner squares which go wrong when algo[0] is a '#'
-    # Should solve this for the general case someday, but the previous two days have taken too much time
-    if i % 2 == 1 and algo[0] == '#':
-        for c in [(-100, -100), (-100, 200), (200, -100), (200, 200)]:
-            grid[c] = '.'
-
-    # print()
+    grid, background = tick(grid, background, algo)
     # grid.print()
-    print(len(grid.index_repeating_cells({"#"})['#']))
-    print()
 
+print(len(grid.index_repeating_cells({"#"})['#']))
