bd1ebeb2d33e258f257ff904a2ea95bf981949c9
Chris Glencross <chris@glencross.org>
2021-12-25 00:01:42 +0000

    2021 day 24

---
+++ aoc2021/day24/day24-as-python.py
@@ -0,0 +1,67 @@
+def toBase26(n):
+    if n == 0:
+        return "0"
+    digits = []
+    while n:
+        digits.append(str(int(n % 26)))
+        n //= 26
+    return "_".join(digits[::-1])
+
+
+def evaluate(inp_0, inp_1, inp_2, inp_3, inp_4, inp_5, inp_6, inp_7, inp_8, inp_9,
+             inp_10, inp_11, inp_12, inp_13):
+
+    # Annotated and tidied version of the output from the decompiler
+    # with some debug print statements to help work out max and min values manually
+
+    z_8 = (((((inp_0 + 1) * 26) + (inp_1 + 9)) * 26) + (inp_2 + 11))
+    print(toBase26(z_8))
+
+    x_23 = (((z_8 % 26) + -13) != inp_3)
+    z_11 = (((z_8 // 26) * ((25 * x_23) + 1)) + ((inp_3 + 6) * x_23))
+    print(toBase26(z_11))
+
+    x_29 = (((z_11 % 26) + 11) != inp_4)
+    z_14 = ((z_11 * ((25 * x_29) + 1)) + ((inp_4 + 6) * x_29))
+    print(toBase26(z_14))
+
+    x_35 = (((z_14 % 26) + 15) != inp_5)
+    z_17 = ((z_14 * ((25 * x_35) + 1)) + ((inp_5 + 13) * x_35))
+    print(toBase26(z_17))
+
+    x_41 = (((z_17 % 26) + -14) != inp_6)
+    z_20 = (((z_17 // 26) * ((25 * x_41) + 1)) + ((inp_6 + 13) * x_41))
+    print(toBase26(z_20))
+
+    x_47 = (((z_20 % 26) + 12) != inp_7)
+    z_23 = ((z_20 * ((25 * x_47) + 1)) + ((inp_7 + 5) * x_47))
+    print(toBase26(z_23))
+
+    x_53 = (((z_23 % 26) + -8) != inp_8)
+    z_26 = (((z_23 // 26) * ((25 * x_53) + 1)) + ((inp_8 + 7) * x_53))
+    print(toBase26(z_26))
+
+    x_59 = (((z_26 % 26) + 14) != inp_9)
+    z_29 = ((z_26 * ((25 * x_59) + 1)) + ((inp_9 + 2) * x_59))
+    print(toBase26(z_29))
+
+    x_65 = (((z_29 % 26) + -9) != inp_10)
+    z_32 = (((z_29 // 26) * ((25 * x_65) + 1)) + ((inp_10 + 10) * x_65))
+    print(toBase26(z_32))
+
+    x_71 = (((z_32 % 26) + -11) != inp_11)
+    z_35 = (((z_32 // 26) * ((25 * x_71) + 1)) + ((inp_11 + 14) * x_71))
+    print(toBase26(z_35))
+
+    x_77 = (((z_35 % 26) + -6) != inp_12)
+    z_38 = (((z_35 // 26) * ((25 * x_77) + 1)) + ((inp_12 + 7) * x_77))
+    print(toBase26(z_38))
+
+    x_83 = (((z_38 % 26) + -5) != inp_13)
+    z_41 = (((z_38 // 26) * ((25 * x_83) + 1)) + ((inp_13 + 1) * x_83))
+    print(toBase26(z_41))
+
+
+# Numbers worked out manually with paper and debugging through the above code
+print(evaluate(9, 6, 9, 7, 9, 9, 8, 9, 6, 9, 2, 4, 9, 5))
+print(evaluate(5, 1, 3, 1, 6, 2, 1, 4, 1, 8, 1, 1, 4, 1))
---
+++ aoc2021/day24/day24-decompiler.py
@@ -0,0 +1,207 @@
+#!/usr/bin/python3
+# Advent of code 2021 day 24
+# See https://adventofcode.com/2021/day/24
+
+from collections import defaultdict
+
+# This is a decompiler of the input.txt assembly language to convert
+# the code to Python, which I can then read and run through a debugger
+
+with open("input.txt") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+var_assignments = {"w": -1, "x": -1, "y": -1, "z": -1, "inp": -1}
+def read_renamed(var):
+    if var in var_assignments.keys():
+        suffix = var_assignments[var]
+        if suffix < 0:
+            # never assigned so we know it is zero
+            return 0
+        else:
+            return f"{var}_{suffix}"
+    else:
+        return int(var)
+
+def write_renamed(var):
+    var_assignments[var] += 1
+    return read_renamed(var)
+
+# Register renaming
+instructions = []
+for line in lines:
+    words = line.split(" ")
+    match words:
+        case ["inp", arg1]:
+            out = write_renamed(arg1)
+            instructions.append((out, "inp", write_renamed("inp")))
+        case [op, arg1, arg2]:
+            in1 = read_renamed(arg1)
+            in2 = read_renamed(arg2)
+            out = write_renamed(arg1)
+            instructions.append((out, op, in1, in2))
+        case _:
+            raise ValueError(f"Bad command ${line}")
+
+def build_expressions(instructions, register_expressions):
+
+    def get_register_value(var):
+        if isinstance(var, int):
+            return var
+        else:
+            return register_expressions.get(var, var)
+
+    for instruction in instructions:
+        out, op, *args = instruction
+        if op == "inp":
+            register_expressions[out] = args[0]
+            continue
+
+        arg0 = args[0]
+        arg1 = args[1]
+        val0 = get_register_value(arg0)
+        val1 = get_register_value(arg1)
+
+        p0 = val0 if isinstance(val0, int) else arg0
+        p1 = val1 if isinstance(val1, int) else arg1
+        register_expressions[out] = op, p0, p1
+
+        # Create a synthetic not-equal instruction
+        if op == "eql" and p1 == 0 and (val0, tuple) and val0[0] == "eql":
+            register_expressions[out] = "neq", val0[1], val0[2]
+
+
+def count_register_usage(expressions, reference_count):
+    for expression in expressions:
+        if isinstance(expression, str):
+            reference_count[expression] += 1
+        if isinstance(expression, tuple):
+            count_register_usage(expression[1:], reference_count)
+
+def simplify(expression, expressions):
+    if isinstance(expression, str) and isinstance(expressions.get(expression), int):
+        return expressions.get(expression)
+    if isinstance(expression, tuple):
+        op, *args = expression
+        for i, arg in enumerate(args):
+            args[i] = simplify(arg, expressions)
+        if all(isinstance(arg, int) for arg in args):
+            if op == 'mul':
+                return args[0] * args[1]
+            if op == 'add':
+                return args[0] + args[1]
+            if op == 'mod':
+                return args[0] % args[1]
+        if op in ["mul", "div", "mod"] and args[0] == 0:
+            return 0
+        if op == "mul" and args[1] == 0:
+            return 0
+        if op == "mul" and args[1] == 1:
+            return args[0]
+        if op == "add" and args[0] == 0:
+            return args[1]
+        if op == "add" and args[1] == 0:
+            return args[0]
+        return op, *args
+    else:
+        return expression
+
+
+def inline_expressions(expressions: dict):
+
+    # Remove common subexpressions
+    common_subexpressions = dict()
+    for target, expression in expressions.items():
+        if expression not in common_subexpressions.keys():
+            common_subexpressions[expression] = target
+
+    for target, expression in list(expressions.items()):
+        if isinstance(expression, tuple):
+            op, *args = expression
+            for i, arg in enumerate(args):
+                if not isinstance(arg, int):
+                    arg_value = expressions.get(arg, arg)
+                    common_subexpression = common_subexpressions.get(arg_value)
+                    if common_subexpression:
+                        args[i] = common_subexpression
+            expressions[target] = (op, *args)
+
+    # Inline expressions which contain simple values
+    for target, expression in list(expressions.items()):
+        if isinstance(expression, str):
+            expressions[target] = expressions.get(expression, expression)
+        if isinstance(expression, tuple):
+            op, *args = expression
+            for i, arg in enumerate(args):
+                if not isinstance(arg, int):
+                    arg_value = expressions.get(arg, arg)
+                    if not isinstance(arg_value, tuple):
+                        args[i] = arg_value
+            expressions[target] = (op, *args)
+
+
+    # Simplify
+    for target, expression in list(expressions.items()):
+        simplified_expression = simplify(expression, expressions)
+        if simplified_expression != expression:
+            expressions[target] = simplified_expression
+
+    # Count how many times each register is used
+    reference_count = defaultdict(lambda: 0)
+    reference_count[read_renamed("z")] = 1
+    count_register_usage(expressions.values(), reference_count)
+
+    # Inline complex expressions which are used only once
+    for target, expression in list(expressions.items()):
+        if isinstance(expression, tuple):
+            op, *args = expression
+            for i, arg in enumerate(args):
+                if isinstance(arg, str) and reference_count[arg] == 1:
+                    arg_value = expressions.get(arg, arg)
+                    args[i] = arg_value
+            expressions[target] = (op, *args)
+
+    # Count how many times each register is used
+    reference_count = defaultdict(lambda: 0)
+    reference_count[read_renamed("z")] = 1
+    count_register_usage(expressions.values(), reference_count)
+
+    # If register is not used, remove it
+    for target, expression in list(expressions.items()):
+        if reference_count[target] == 0:
+            expressions.pop(target)
+
+
+register_expressions = {}
+build_expressions(instructions, register_expressions)
+
+# These were set by manual inspection
+register_expressions.update({
+    'x_5': 1,
+    'x_11': 1,
+    'x_17': 1
+})
+inline_expressions(register_expressions)
+inline_expressions(register_expressions)
+inline_expressions(register_expressions)
+
+def print_expression(expression):
+    if isinstance(expression, tuple):
+        op, *args = expression
+        infix_operators = {
+            "eql": "==",
+            "neq": "!=",
+            "mul": "*",
+            "add": "+",
+            "div": "//",
+            "mod": "%"
+        }
+        if op in infix_operators.keys():
+            return "(" + print_expression(args[0]) + " " + infix_operators[op] + " " + print_expression(args[1]) + ")"
+        else:
+            return str(op)
+    else:
+        return str(expression)
+
+for target, expression in register_expressions.items():
+    print(f"{target} = {print_expression(expression)}")
+
