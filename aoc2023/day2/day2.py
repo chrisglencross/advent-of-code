8351185da0d5739d85788b58b4aa5a1648936842
Chris Glencross <chris@glencross.org>
2023-12-02 18:17:12 +0000

    2023 day 2

---
+++ aoc2023/day2/day2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Advent of code 2023 day 2
+# See https://adventofcode.com/2023/day/2
+from functools import reduce
+from itertools import zip_longest
+from operator import mul
+import re
+
+with open("input.txt") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+total = 0
+power = 0
+
+for line in lines:
+
+    game, reveals = re.match("^Game ([0-9]+): (.*)$", line).groups()
+    draws = [{d.split(" ")[1]: int(d.split(" ")[0]) for d in cubes.split(", ")} for cubes in reveals.split("; ")]
+    rgbs = [[draw.get(color, 0) for color in ["red", "green", "blue"]] for draw in draws]
+
+    if all(component <= limit for rgb in rgbs for component, limit in zip_longest(rgb, (12, 13, 14))):
+        total += int(game)
+
+    min_rgb = reduce(lambda rgb1, rgb2: [max(p) for p in zip_longest(rgb1, rgb2)], rgbs)
+    power += reduce(mul, min_rgb)
+
+print(total)
+print(power)
