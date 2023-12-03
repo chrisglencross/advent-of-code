6e1294efcae8cae25904f9c9f02022e618b2d990
Chris Glencross <chris@glencross.org>
2023-12-03 21:39:10 +0000

    2023 day 3

---
+++ aoc2023/day3/day3.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# Advent of code 2023 day 3
+# See https://adventofcode.com/2023/day/3
+import itertools
+from aoc2023.modules import grid as g
+from aoc2023.modules import directions as d
+
+grid = g.load_grid("input.txt")
+digits = grid.index_repeating_cells("0123456789")
+digit_locations = {l for ls in digits.values() for l in ls}
+
+symbols = grid.index_repeating_cells(not_symbols="0123456789.")
+symbol_locations = {l for ls in symbols.values() for l in ls}
+
+# Part 1 (plus indexing number locations for Part 2)
+total = 0
+number_start_locations = {(x, y) for x, y in digit_locations if (x - 1, y) not in digit_locations}
+number_locations = {}
+for number_start_location in number_start_locations:
+    x, y = number_start_location
+    number = 0
+    adjacent_symbol = False
+    number_digit_locations = []
+    while grid.get((x, y), '.') in "0123456789":
+        number_digit_locations.append((x, y))
+        number = 10 * number + int(grid[(x, y)])
+        if any([grid.get(dir.move((x, y)), '.') not in "0123456789." for dir in d.COMPASS_DIRECTIONS_8.values()]):
+            adjacent_symbol = True
+        x += 1
+    if adjacent_symbol:
+        total += number
+    for c in number_digit_locations:
+        number_locations[c] = (number_start_location, number)
+print(total)
+
+# Part 2
+stars = grid.find_cells("*")
+star_adjacent_locations = [(star, direction.move(star))
+                           for star in stars
+                           for direction in d.COMPASS_DIRECTIONS_8.values()]
+star_adjacent_numbers = {(star, number_locations[l])
+                         for star, l in star_adjacent_locations
+                         if l in number_locations.keys()}
+numbers_grouped_by_adjacent_star = {star: [n for _, n in numbers]
+                                    for star, numbers in itertools.groupby(sorted(star_adjacent_numbers), lambda n: n[0])}
+number_pairs = [numbers for numbers in numbers_grouped_by_adjacent_star.values() if len(numbers) == 2]
+print(sum(n1 * n2 for (_, n1), (_, n2) in number_pairs))
---
+++ aoc2023/modules/directions.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Tuple, Dict
+
+
+@dataclass
+class Direction:
+    name: str
+    left_name: str
+    right_name: str
+    delta: Tuple[int, int] = field(default_factory=None)
+    directions: Dict[str, object] = field(default_factory=None)
+
+    def move(self, from_location, step=1):
+        return from_location[0] + self.delta[0] * step, from_location[1] + self.delta[1] * step
+
+    def turn_left(self, turns=1):
+        result = self
+        for i in range(0, turns):
+            result = result.directions[result.left_name]
+        return result
+
+    def turn_right(self, turns=1):
+        result = self
+        for i in range(0, turns):
+            result = result.directions[result.right_name]
+        return result
+
+    def reverse(self) -> Direction:
+        return self.turn_right().turn_right()
+
+# Directions with left and right turns
+UDLR_DIRECTIONS = {}
+UDLR_DIRECTIONS["U"] = Direction(name="U", delta=(0, -1), left_name="L", right_name="R",
+                                    directions=UDLR_DIRECTIONS)
+UDLR_DIRECTIONS["R"] = Direction(name="R", delta=(1, 0), left_name="U", right_name="D",
+                                    directions=UDLR_DIRECTIONS)
+UDLR_DIRECTIONS["D"] = Direction(name="D", delta=(0, 1), left_name="R", right_name="L",
+                                    directions=UDLR_DIRECTIONS)
+UDLR_DIRECTIONS["L"] = Direction(name="L", delta=(-1, 0), left_name="D", right_name="U",
+                                    directions=UDLR_DIRECTIONS)
+
+# Directions with left and right turns
+COMPASS_DIRECTIONS = {}
+COMPASS_DIRECTIONS["N"] = Direction(name="N", delta=(0, -1), left_name="W", right_name="E",
+                                    directions=COMPASS_DIRECTIONS)
+COMPASS_DIRECTIONS["E"] = Direction(name="E", delta=(1, 0), left_name="N", right_name="S",
+                                    directions=COMPASS_DIRECTIONS)
+COMPASS_DIRECTIONS["S"] = Direction(name="S", delta=(0, 1), left_name="E", right_name="W",
+                                    directions=COMPASS_DIRECTIONS)
+COMPASS_DIRECTIONS["W"] = Direction(name="W", delta=(-1, 0), left_name="S", right_name="N",
+                                    directions=COMPASS_DIRECTIONS)
+
+COMPASS_DIRECTIONS_8 = {}
+COMPASS_DIRECTIONS_8["N"] = Direction(name="N", delta=(0, -1), left_name="NW", right_name="NE",
+                                      directions=COMPASS_DIRECTIONS_8)
+COMPASS_DIRECTIONS_8["E"] = Direction(name="E", delta=(1, 0), left_name="NE", right_name="SE",
+                                      directions=COMPASS_DIRECTIONS_8)
+COMPASS_DIRECTIONS_8["S"] = Direction(name="S", delta=(0, 1), left_name="SE", right_name="SW",
+                                      directions=COMPASS_DIRECTIONS_8)
+COMPASS_DIRECTIONS_8["W"] = Direction(name="W", delta=(-1, 0), left_name="SW", right_name="NW",
+                                      directions=COMPASS_DIRECTIONS_8)
+COMPASS_DIRECTIONS_8["NE"] = Direction(name="NE", delta=(1, -1), left_name="N", right_name="E",
+                                       directions=COMPASS_DIRECTIONS_8)
+COMPASS_DIRECTIONS_8["SE"] = Direction(name="SE", delta=(1, 1), left_name="E", right_name="S",
+                                       directions=COMPASS_DIRECTIONS_8)
+COMPASS_DIRECTIONS_8["SW"] = Direction(name="SW", delta=(-1, 1), left_name="S", right_name="W",
+                                       directions=COMPASS_DIRECTIONS_8)
+COMPASS_DIRECTIONS_8["NW"] = Direction(name="NW", delta=(-1, -1), left_name="W", right_name="N",
+                                       directions=COMPASS_DIRECTIONS_8)
