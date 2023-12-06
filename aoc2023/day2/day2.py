127aa3a0d3de44f9d6145e16e0e5387f48bb1405
Chris Glencross <chris@glencross.org>
2023-12-06 08:38:18 +0000

    Fix variable name

--- aoc2023/day2/day2.py
+++ aoc2023/day2/day2.py
@@ -20,8 +20,8 @@ for line in lines:
     if all(component <= limit for rgb in rgbs for component, limit in zip(rgb, (12, 13, 14))):
         total += int(game)
 
-    min_rgb = reduce(lambda rgb1, rgb2: [max(p) for p in zip(rgb1, rgb2)], rgbs)
-    power += reduce(mul, min_rgb)
+    max_rgb = reduce(lambda rgb1, rgb2: [max(p) for p in zip(rgb1, rgb2)], rgbs)
+    power += reduce(mul, max_rgb)
 
 print(total)
 print(power)
