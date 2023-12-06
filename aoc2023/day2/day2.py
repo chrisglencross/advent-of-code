6b03adc2a64b37fa51809bc54c535d3f33fa29cb
Chris Glencross <chris@glencross.org>
2023-12-06 08:36:51 +0000

    Replace zip_longest with zip

--- aoc2023/day2/day2.py
+++ aoc2023/day2/day2.py
@@ -2,7 +2,6 @@
 # Advent of code 2023 day 2
 # See https://adventofcode.com/2023/day/2
 from functools import reduce
-from itertools import zip_longest
 from operator import mul
 import re
 
@@ -18,10 +17,10 @@ for line in lines:
     draws = [{d.split(" ")[1]: int(d.split(" ")[0]) for d in cubes.split(", ")} for cubes in reveals.split("; ")]
     rgbs = [[draw.get(color, 0) for color in ["red", "green", "blue"]] for draw in draws]
 
-    if all(component <= limit for rgb in rgbs for component, limit in zip_longest(rgb, (12, 13, 14))):
+    if all(component <= limit for rgb in rgbs for component, limit in zip(rgb, (12, 13, 14))):
         total += int(game)
 
-    min_rgb = reduce(lambda rgb1, rgb2: [max(p) for p in zip_longest(rgb1, rgb2)], rgbs)
+    min_rgb = reduce(lambda rgb1, rgb2: [max(p) for p in zip(rgb1, rgb2)], rgbs)
     power += reduce(mul, min_rgb)
 
 print(total)
