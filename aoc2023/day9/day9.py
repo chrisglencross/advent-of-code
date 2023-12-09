d0e2c5608189f61bf4ea8a50fe19ac521c564deb
Chris Glencross <chris@glencross.org>
2023-12-09 08:39:17 +0000

    2023 day 9

--- aoc2023/day9/day9.py
+++ aoc2023/day9/day9.py
@@ -3,11 +3,11 @@
 # See https://adventofcode.com/2023/day/9
 
 def calculate_next(row):
-    rows = [row]
+    n = 0
     while not all(v == 0 for v in row):
+        n += row[-1]
         row = [v2 - v1 for v1, v2 in zip(row, row[1:])]
-        rows.append(row)
-    return sum([row[-1] for row in rows])
+    return n
 
 
 with open("input.txt") as f:
